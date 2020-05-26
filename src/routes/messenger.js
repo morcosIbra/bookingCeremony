d128d5c55a2878be4981a67c46f75854f83a058a
morcos daniel <mdanielibra@gmail.com>
2020-05-26 22:59:54 +0200

    remove unique from userform

--- src/routes/messenger.js
+++ src/routes/messenger.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { verify, communicate } from '../controllers/messenger';
 const router = express.Router();
 
-router.get('/', verify);
+router.get('', verify);
 router.post('', communicate);
 
 export default router;
