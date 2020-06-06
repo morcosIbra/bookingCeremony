36fa32873982dce98ba24598cb3b7386e2860f35
MarioDou7 <mariomamdouhreda@gmail.com>
2020-06-06 22:41:22 +0200

    phase

--- src/db/index.js
+++ src/db/index.js
@@ -3,6 +3,7 @@ import UserForm from './models/userForm';
 import Confession from './models/confession';
 import Holymass from './models/holyMass';
 import ChurchMember from './models/churchMember';
+import Phase from './models/phase';
 import connectDb from './config';
 
-export { UserForm,Confession,Holymass,ChurchMember, connectDb };
+export { UserForm,Confession,Holymass,ChurchMember,Phase, connectDb };
