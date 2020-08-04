f42ccda404e8f687c260ab9343b1237b2d3c0ba3
Morcos <mdanielibra@gmail.com>
2020-08-04 14:23:56 +0200

    admin credentials

--- client/src/store/reducers/auth.js
+++ client/src/store/reducers/auth.js
@@ -12,8 +12,8 @@ const initialState = {
     },
     isAdmin: false,
     rightCredentials: {
-        username: 'admin',
-        password: 'admin'
+        username: 'stgmb',
+        password: 'stgmb10@'
     }
 }
 const auth = (state = initialState, action) => {
