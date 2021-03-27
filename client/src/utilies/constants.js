dbe2eaa8d0e0fa23474f5f7d1c21b3b91f1ff127
GeorgeKBarsoum <37223320+GeorgeKBarsoum@users.noreply.github.com>
2021-03-27 14:07:10 +0200

    Update constants.js
    Sick prayers edit

--- client/src/utilies/constants.js
+++ client/src/utilies/constants.js
@@ -32,8 +32,8 @@ export const loginFailed = 'من فضلك تأكد من اسم المستخدم 
 export const homeTitle = 'حجز القداسات';
 export const holymasses = 'قداسات';
 export const holymass = 'قداس';
-export const eveningPrayer = 'عشيه';
-export const eveningPrayers = 'عشيات';
+export const eveningPrayer = 'صلاة مسحة المرضة';
+export const eveningPrayers = 'صلوات مسحة المرضي';
 export const availableCeremonies = ceremony =>
     ceremony === 'holymass' ? `${holymasses} المتاحه حاليا` : `${eveningPrayers} المتاحه حاليا`
 
