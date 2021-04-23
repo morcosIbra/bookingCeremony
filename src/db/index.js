23cdb6398c77ab6550ce7fe6fbf2eeff85317e65
Morcos <mdanielibra@gmail.com>
2021-04-23 15:59:49 +0200

    add pascha module

--- src/db/index.js
+++ src/db/index.js
@@ -2,10 +2,11 @@ import mongoose from 'mongoose';
 import UserForm from './models/userForm';
 import Confession from './models/confession';
 import Holymass from './models/holyMass';
+import Pascha from './models/pascha';
 import EveningPrayer from './models/eveningPrayer';
 import ChurchMember from './models/churchMember';
 import Phase from './models/phase';
 import Reservation from './models/reservation';
 import connectDb from './config';
 
-export { UserForm,Confession,Holymass,EveningPrayer,ChurchMember,Phase,Reservation, connectDb };
+export { UserForm,Confession,Holymass,Pascha,EveningPrayer,ChurchMember,Phase,Reservation, connectDb };
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,7 @@
 import messenger from './messenger';
 import holymass from './holymass';
 import eveningPrayer from './eveningPrayer';
+import pascha from './pascha';
 import confession from './confession';
 import churchmember from './churchmember';
 import phase from './phase';
@@ -8,6 +9,7 @@ import phase from './phase';
 const routes = function (app) {
   app.use('/messenger', messenger);
   app.use('/holymass', holymass);
+  app.use('/pascha', pascha);
   app.use('/confession', confession);
   app.use('/churchmember', churchmember);
   app.use('/phase', phase);
