4c291607ba13a1679649e59e9df4ad9f8f43fb3c
GeorgeKBarsoum <37223320+GeorgeKBarsoum@users.noreply.github.com>
2021-04-24 15:16:42 +0200

    Update constants.js

--- client/src/utilies/constants.js
+++ client/src/utilies/constants.js
@@ -30,8 +30,8 @@ export const inputText = {
 };
 export const loginFailed = 'من فضلك تأكد من اسم المستخدم و كلمه السر'
 export const homeTitle = 'حجز القداسات';
-export const holymasses = 'قداسات';
-export const holymass = 'قداس';
+export const holymasses = 'بصخات صباحية';
+export const holymass = 'بصخة صباحية';
 export const eveningPrayer = 'مناسبة'
 export const eveningPrayers = 'مناسبات';
 export const pascha = 'بصخه'
